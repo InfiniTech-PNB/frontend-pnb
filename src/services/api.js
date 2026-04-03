2ce7de661f2f3dd03eacb2b57aae036d240fd3bf
akshay060805 <vanama.akshay06@gmail.com>
2026-04-04 01:16:22 +0530

    api url changed

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     // baseURL: "http://localhost:3000/api",
-    baseURL: "http://ec2-98-130-120-168.ap-south-2.compute.amazonaws.com:3000/api",
+    baseURL: "http://ec2-40-192-14-155.ap-south-2.compute.amazonaws.com:3000/api",
     withCredentials: true
 });
 
